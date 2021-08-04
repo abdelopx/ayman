49d42ad4e0c487c945017d9e42655df2de542a3f
k3yn <abdelpro1236@gmail.com>
2021-08-03 21:37:56 -0400

    fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
         </SectionTitle>
         <SectionText>
 Hey I'm Abdel 👋. Currently a college student I like to craft solid and scalable applications with great user experiences.</SectionText>
-        <Button onClick={props.handleClick}><a style={{color: "white"}} href="https://srv-store3.gofile.io/download/df33cd92-3e90-4d37-a424-01d6afdd36e4/Abdellah's%20Resume%20(1).pdf">Get resume</a></Button>
+<a style={{color: "white"}} href="https://srv-store3.gofile.io/download/df33cd92-3e90-4d37-a424-01d6afdd36e4/Abdellah's%20Resume%20(1).pdf"><Button onClick={props.handleClick}>Get Resume</Button></a>
       </LeftSection>
     </Section>
   </>
